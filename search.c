9850c4109b450faebc5b11e20cb13ffca0a6685e
schaudge <yuanshenran@yeah.net>
2024-12-25 17:03:10 +0800

    update

--- search.c
+++ search.c
@@ -216,7 +216,7 @@ static void write_all_hits(kstring_t *out, const m_seq_t *s, const rb3_swrst_t *
 static void write_per_seq(step_t *t)
 {
 	const pipeline_t *p = t->p;
-	int32_t i, j;
+	int32_t i, j, k;
 	kstring_t out = {0,0,0};
 	for (j = 0; j < t->n_seq; ++j) {
 		m_seq_t *s = &t->seq[j];
@@ -237,7 +237,7 @@ static void write_per_seq(step_t *t)
 				rb3_sprintf_lite(&out, "\t%d\t*\t*\t*\t*\t*\t*\t*\t0\t0\t0", s->len);
                 if (p->opt->swo.flag & RB3_SWF_MAX_HIS) {
                     rb3_sprintf_lite(&out, "\tAS:i:0\tqh:i:1\trh:i:0\tcg:Z:*\tcs:Z:*\ths:Z:*\thc:Z:*\tqs:Z:");
-                    for (int32_t k = 0; k < s->len; ++k)
+                    for (k = 0; k < s->len; ++k)
                         rb3_sprintf_lite(&out, "%c", "$ACGTN"[s->seq[k]]);
                 }
                 rb3_sprintf_lite(&out, "\n");
